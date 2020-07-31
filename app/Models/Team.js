8076bef1d261c9ee52c65c5163146032c0921e5b
Zartosht Sepideman <xashayarsha@gmail.com>
2020-07-31 17:46:33 +0430

    add extra data to team matches show

--- app/Models/Team.js
+++ app/Models/Team.js
@@ -87,12 +87,30 @@ class Team extends Model {
   async matches() {
     const Database = use("Database"),
       id = this.id
+
+    let matchesList = await Database.table("matches").where(function () {
+      this.where('first_team_id', id)
+        .orWhere('second_team_id', id)
+    })
+
+    let matches = []
+    const teams = await Team.all()
+    for (const match of matchesList) {
+      const first_team = teams.toJSON().filter((team) => { return team.id === match.first_team_id })[0],
+        second_team = teams.toJSON().filter((team) => { return team.id === match.second_team_id })[0],
+        at = match.at === match.first_team_id ? first_team : match.at === match.secont_team_id ? second_team : await Team.find(match.at)
+
+      matches.push({
+        ...match,
+        first_team,
+        second_team,
+        at
+      })
+    }
+
     return {
       team: this,
-      matches: await Database.table("matches").where(function () {
-        this.where('first_team_id', id)
-          .orWhere('second_team_id', id)
-      })
+      matches: matches
     }
   }
 }
